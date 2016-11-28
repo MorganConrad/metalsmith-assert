ab29810d02b2ecdef586431fec4964b0450b3c2f
Morgan Conrad <flyingspaniel@gmail.com>
2016-11-28 14:07:30 -0800

    v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # metalsmith-assert
-Metalsmith plugin for testing with assert.  Not sure hoe useful this will be to others.
+Metalsmith plugin for testing the file objects with Node's [assert](https://nodejs.org/api/assert.html) module.  
+
+### Usage
+
+Install as usual,  `npm install metalsmith-assert`.
+
+Javascript:  `use(metalsmith-assert(assertions, options))`
+
+CLI: Haven't tested it yet.  You'd lose a few options since it can't support functions.
+
+By default, **metalsmith-assert** will console.log all AssertionErrors and a summary line.
+It then calls `done(error) with the last AssertionError (if any) or `null` if all is well.
+
+#### Assertions
+
+This should be an object containing objects of the following format:
+
+    testname : {  // used for the message
+      assert:     // which method of assert to call, null -> "ok".  See below.
+      actual:     // if a string, the use fileData[actual],
+                  // else call actual(fileData, filePath, metalsmith)
+      expected:   // if a string use it
+                  // else call actual(fileData, filePath, metalsmith)
+                  // note: ignored if assert is "ok"
+    }
+
+##### Legal values for assert method are all methods from [assert](https://nodejs.org/api/assert.html) that take "actual" as the first argument.
+
+ - deepEqual(actual, expected, ...)
+ - deepStrictEqual(actual, expected, ...)
+ - equal(actual, expected, ...)
+ - notDeepEqual(actual, expected, ...)
+ - notDeepStrictEqual(actual, expected, ...)
+ - notEqual(actual, expected, ...)
+ - notStrictEqual(actual, expected, ...)
+ - ok(actual, ...)
+ - strictEqual(actual, expected, ...)
+
+Be precise in your spelling and capitalization.  You can also use an array of objects, in which case the testname will be "test N" where N in the index in the array.
+
+
+#### Options
+
+**options.quiet**  Don't log individual failures, nor the summary of passed/failed
+
+**options.continueBuildOnError** Always call `done(null)` even if there was a failure.
+
+ - If you enable _both_ quiet and continueBuildOnError this module is useless.  :-)
+
+**options.disable** a quick way to turn metalsmith-assert off - it will immediately call done().
+
+**options.fileFilter** determines which files will be included
+ - if missing, include all files.
+ - if a string or Regex, only include matching filePaths.
+ - if a user-provided-function, include the file when `filter(filePath, data, metalsmith)` returns true.  
+ _e.g._ If you want to use [multimatch](https://www.npmjs.com/package/multimatch), pass something like `function(filePath) { return multimatch([filePath], ["blogs/**", ...])[0] };`
+
+
+### Examples
+
+To verify that every blog entry has a title, and a value for "tag" === "vacation"
+
+    .use(metalsmith-assert({
+          "title exists" : { actual: "title"},
+          "tag === blog" : { assert: "equal", actual: "tag", expected: "vacation" }
+        } ) )
+
+
+
+### Notes, Todos, and Caveats
---
+++ assert.js
@@ -0,0 +1,120 @@
+var nodeAssert = require('assert');
+
+module.exports = metalsmithAssert;
+
+/**
+ * Metalsmith plugin that prints the objects
+ *
+ * @param {Object} opts
+ * @return {Function}
+ */
+
+function metalsmithAssert(assertions, options) {
+
+   if (Array.isArray(assertions)) {
+      var convertToHash = {};
+      for (var i=0; i<assertions.length; i++)
+         convertToHash["test " + i] = assertions[i];
+
+      assertions = convertToHash;
+   }
+
+  options = normalize(options);
+
+  return function(files, metalsmith, done) {
+
+     if (options.disable)
+        return done();
+
+     var filesTested = 0;
+     var failed = 0;
+     var passed = 0;
+     var lastError;
+     try {
+        Object.keys(files).forEach(function(filePath) {
+           var fileData = files[filePath];
+           if (options.fileFilter(filePath, fileData, metalsmith)) {
+               filesTested++;
+               Object.keys(assertions).forEach(function(testname) {
+                  var v = assertions[testname];
+                  var method = v['assert'] || 'ok';
+                  var actual = v['actual'];
+                  if (typeof actual === 'string')
+                     actual = fileData[actual];
+                  else
+                     actual = actual(fileData, filePath, metalsmith);
+
+                  var expected = v['expected'];
+                  if (typeof expected === "function")
+                     expected = expected(fileData, filePath, metalsmith);
+
+                  var message = v['message'] || testname;
+                  var message = filePath + ': test failed: ' + message;
+
+                  try {
+                     if ('ok' === method)
+                        nodeAssert[method](actual, message);
+                     else
+                        nodeAssert[method](actual, expected, message);
+                     passed++;
+                  }
+                  catch (e) {
+                     if (!options.quiet)
+                        console.log(e);
+                     lastError = e;
+                     failed++;
+                  }
+
+               });
+            }
+         });
+
+         if (!options.quiet)
+            console.log("metalsmith-assert processed " + filesTested + " files.  Passed tests = " + passed + "   Failed tests = " + failed);
+         if (!options.continueBuildOnError)
+            done(lastError);
+         else
+            done();
+
+     }
+     catch (err) {
+        done(err);
+     }
+  };
+}
+
+
+
+/**
+ * Normalize an `options` dictionary.
+ *
+ * @param {Object} options
+ */
+
+ function normalize(options){
+   options = options || { };
+   options.fileFilter = fileFilter(options.fileFilter);
+   return options;
+};
+
+
+/* calculate file filter function */
+function fileFilter(filter) {
+  if (filter) {
+     if (Array.isArray(filter)) {
+        var mm = require('multimatch');
+        return function(filePath) { return mm([filePath],filter); };
+     }
+     else if (typeof filter === 'string') {
+        var regex = new RegExp(filter);
+        return function(filePath) { return regex.test(filePath); }
+     }
+     else if (filter instanceof RegExp)
+        return function(filePath) { return filter.test(filePath); }
+     else   // must be a function itself
+        return filter;
+   }
+   else // none, return "pass all"
+      return function() { return true; };
+
+}
